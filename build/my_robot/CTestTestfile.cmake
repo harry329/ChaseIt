9be4b41542e0ebf063cd44f8018ac260b6c0541c
Ubuntu <ubuntu@ip-172-31-1-83.us-west-2.compute.internal>
2023-11-22 20:56:20 +0000

    Camera & Lidar link added

--- build/my_robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/Desktop/catkin_ws/ChaseIt/src/my_robot
-# Build directory: /home/ubuntu/Desktop/catkin_ws/ChaseIt/build/my_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
